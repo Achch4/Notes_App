5d84c2bc2f04e1a79ae28aef6b232abb8cbdf3af
Achch4 <sampathachintha@gmail.com>
2026-05-04 22:35:32 +0530

    added search feature to search notes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { PlusIcon } from "lucide-react";
 import React from "react";
 import { Link } from "react-router";
 
-const Navbar = () => {
+const Navbar = ({ searchQuery, setSearchQuery }) => {
   return (
     <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4">
@@ -10,6 +10,12 @@ const Navbar = () => {
           <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
             Thinkboard
           </h1>
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
           <div className="flex items-center gap-4">
             <Link to={"/create"} className="btn btn-primary">
               <PlusIcon className="size-5" />
@@ -21,5 +27,4 @@ const Navbar = () => {
     </header>
   );
 };
-
 export default Navbar;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,62 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/Navbar'
-import RateLimitedUI from '../components/RateLimitedUI';
-import NoteCard from '../components/NoteCard';
-import NotesNotFound from '../components/NotesNotFound';
-import toast from 'react-hot-toast';
-import api from '../libs/axios';
-
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import RateLimitedUI from "../components/RateLimitedUI";
+import NoteCard from "../components/NoteCard";
+import NotesNotFound from "../components/NotesNotFound";
+import toast from "react-hot-toast";
+import api from "../libs/axios";
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const[isRateLimited, setIsRateLimited] = useState(false);
-  const[notes,setNotes] = useState([]);
-  const[loading,setLoading] = useState(true);
-
-  useEffect(()=> {
+  useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await api.get("/notes");//calling axios directly
+        const res = await api.get("/notes"); //calling axios directly
         console.log(res.data);
-        setNotes(res.data)
-        setIsRateLimited(false)
+        setNotes(res.data);
+        setIsRateLimited(false);
       } catch (error) {
         console.log("error fetching notes");
         console.log(error);
-        if(error.response?.status === 429){
-          setIsRateLimited(true)
-        }else{
-          toast.error("failed to import notes")
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error("failed to import notes");
         }
-
-      }finally{
-        setLoading(false)
+      } finally {
+        setLoading(false);
       }
     };
     fetchNotes();
-  },[]);
+  }, []);
 
+  const filteredNotes = notes.filter(
+    (note) =>
+      note.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+      note.content.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
   return (
-    <div className='min-h-screen'>
-      <Navbar/>
-      {isRateLimited && <RateLimitedUI/>}
+    <div className="min-h-screen">
+      <Navbar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      {isRateLimited && <RateLimitedUI />}
 
-      <div className='max-w-7xl mx-auto p-4 mt-6'>
-      {loading && <div className='text-center text-primary py-10'>Loading notes....</div>}
-      {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
-      {notes.length>0 && !isRateLimited && (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-            {notes.map(note => (
-            <NoteCard key={note._id} note={note} setNotes={setNotes}/>
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">
+            Loading notes....
+          </div>
+        )}
+        {notes.length === 0 && !isRateLimited && <NotesNotFound />}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {filteredNotes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
-
-        </div>
-      )}
+          </div>
+        )}
+      </div>
     </div>
-    </div>
+  );
+};
 
-  )
-}
-
-export default HomePage
+export default HomePage;
