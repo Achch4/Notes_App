9ffc328c762014f560544da5ed093943d172aca8
Achch4 <sampathachintha@gmail.com>
2026-05-11 22:21:08 +0530

    UI: updated ui with colors to fit the theme

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,9 +8,9 @@ const Navbar = ({ searchQuery, setSearchQuery }) => {
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
-            Thinkboard
+            Notepad
           </h1>
-          <input
+          <input className="p-3 rounded-2xl bg-[#c2148e80]"
             type="text"
             placeholder="Search..."
             value={searchQuery}
